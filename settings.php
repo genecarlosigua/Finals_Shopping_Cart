e4441e0a1ffedad433a16f7d56aad79110e9a3b9
genecarlosigua <genecarlosigua22@gmail.com>
2022-12-10 21:20:19 +0800

    Dashboard/home Design

--- settings.php
+++ settings.php
@@ -6,19 +6,11 @@
   
 <?php include("header.php"); ?>
 
-<div class="container-fluid">
-    <div class="row">
-    
         <main class="col-md-9 ms-sm-auto col-lg-12 px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2"><i class="fa fa-dashboard"></i> Dashboard </h1>
+                <h1 class="h2"><i class="fa-solid fa-gear"></i> Settings</h1>
             </div>
-
-                <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>   
-                 
         </main>
-    </div>
-</div>
 
 <?php include("footer.php"); ?>
 
