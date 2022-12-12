88848daa72f01b03c5803283a66a94670d8d27e3
genecarlosigua <genecarlosigua22@gmail.com>
2022-12-12 13:25:51 +0800

    Last Commit

--- settings.php
+++ settings.php
@@ -1,21 +1,57 @@
 <?php
     session_start();
-
-    if(isset($_SESSION['id']) && ($_SESSION['username'])) {
+    if (isset($_SESSION['id'])&& isset($_SESSION['username'])) {
 ?>
-  
-<?php include("header.php"); ?>
 
-        <main class="col-md-9 ms-sm-auto col-lg-12 px-md-4">
-            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 class="h2"><i class="fa-solid fa-gear"></i> Settings</h1>
-            </div>
-        </main>
+<?php require_once("header.php"); 
 
-<?php include("footer.php"); ?>
+    if(isset($_POST['btnchange'])) {
+                
+        $userpass = sanitizeInput($con, $_POST['txtpass']);  
+
+        $err = [];
+
+        if(empty($userpass))
+            $err[] = "Product Name is required!";  
+        if(empty($err)){
+            
+            $strSql = "UPDATE user_table SET
+
+                                password = '$userpass',  
+
+                        WHERE product_ID = " . $_SESSION['k'];
+
+            if(mysqli_query($con, $strSql))
+                ;
+            else
+                echo 'Error: Failed to Update Record!';          
+        }              
+        closeConnection($con);         
+    }
+
+    ?>
+                <main class="col-md-6 ms-sm-auto col-lg-12 px-md-4">
+                    <h2><i class="fa-solid fa-gear"></i> Settings</h2>
+
+                        <div class="form-group row">
+                            <label for="txtname" class="col-sm-2 col-form-label"><h4><b> Change Password</b></h4></label>
+                            <div class="col-sm-10">
+                                <input type="text" name="txtpass" class="form-control" id="txtname" value="">
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                                <div class=" col-sm-10">
+                                    <a href="home.php" class="btn btn-primary"><i class="fa-solid fa-arrow-left-long"></i> Back </a>
+                                    <button type="submit" name="btnchange" class="btn btn-danger"> Change Password</button>
+                                </div>
+                        </div>
+                </main>
+
+
+<?php require_once("footer.php"); ?>
 
 <?php
-
     }else{
         header("Location: index.php");
         exit();
